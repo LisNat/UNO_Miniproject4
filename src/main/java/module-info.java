50c3bbc3c4c6f728da0bb70ede03f37e41ee8c54
davidtaborda2025 <taborda.david@correounivalle.edu.co>
2025-07-31 15:12:43 -0500

    Final version with the unitary test functional.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module org.example.eiscuno {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-    requires org.junit.jupiter.api;
+    //requires org.junit.jupiter.api;
 
 
     opens org.example.eiscuno to javafx.fxml;
